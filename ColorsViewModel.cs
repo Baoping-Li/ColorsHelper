efde54933bffbcda1ac7daef52abd713e85dddfc
Baoping Li <LBaoping@slb.com>
2018-09-04 15:52:11 +0100

    first bunch

---
+++ ColorsViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Media;
+
+namespace ColorsHelper
+{
+    public class BrushItem
+    {
+        public Brush Brush { get; set; }
+        public string Key { get; set; }
+    }
+    public class ColorItem
+    {
+        public Brush Color { get; set; }
+        public string Key { get; set; }
+    }
+    public class ColorsViewModel
+    {
+        private List<BrushItem>  _brushes = new List<BrushItem>();
+        private List<ColorItem>  _colors = new List<ColorItem>();
+
+        public List<BrushItem> Brushes => _brushes;
+        public List<ColorItem> Colors => _colors;
+        public ColorsViewModel()
+        {
+            foreach (var prop in typeof(SystemColors).GetProperties(System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.Public))
+            {
+                if (typeof(Brush).IsAssignableFrom(prop.PropertyType))
+                    _brushes.Add(new BrushItem() { Brush = prop.GetValue(null) as Brush, Key = prop.Name });
+                if (prop.PropertyType == typeof(Color))
+                    _colors.Add(new ColorItem() { Color = new SolidColorBrush((Color)prop.GetValue(null)), Key = prop.Name });
+            }
+        }
+    }
+}
---
+++ InversColorConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace ColorsHelper
+{
+    public class InversColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            SolidColorBrush b = value as SolidColorBrush;
+            if (b == null)
+                return null;
+            Color c = b.Color;
+            return new SolidColorBrush(Color.FromScRgb(1-c.ScA, 255-c.ScR, 255-c.ScG, 255-c.ScB));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
